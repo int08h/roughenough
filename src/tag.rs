270d6a61c91618977e18ba4846e93613a54d097a
Stuart Stock <stuart@int08h.com>
2018-02-24 23:17:34 -0600

    Finally get Tag string byte literals correct

--- src/tag.rs
+++ src/tag.rs
@@ -40,20 +40,20 @@ impl Tag {
     /// Translates a tag into its on-the-wire representation
     pub fn wire_value(&self) -> &'static [u8] {
         match *self {
-            Tag::CERT => "CERT".as_bytes(),
-            Tag::DELE => "DELE".as_bytes(),
-            Tag::INDX => "INDX".as_bytes(),
-            Tag::MAXT => "MAXT".as_bytes(),
-            Tag::MIDP => "MIDP".as_bytes(),
-            Tag::MINT => "MINT".as_bytes(),
-            Tag::NONC => "NONC".as_bytes(),
-            Tag::PAD => [b'P', b'A', b'D', 0xff].as_ref(),
-            Tag::PATH => "PATH".as_bytes(),
-            Tag::PUBK => "PUBK".as_bytes(),
-            Tag::RADI => "RADI".as_bytes(),
-            Tag::ROOT => "ROOT".as_bytes(),
-            Tag::SIG => [b'S', b'I', b'G', 0x00].as_ref(),
-            Tag::SREP => "SREP".as_bytes(),
+            Tag::CERT => b"CERT",
+            Tag::DELE => b"DELE",
+            Tag::INDX => b"INDX",
+            Tag::MAXT => b"MAXT",
+            Tag::MIDP => b"MIDP",
+            Tag::MINT => b"MINT",
+            Tag::NONC => b"NONC",
+            Tag::PAD => b"PAD\xff",
+            Tag::PATH => b"PATH",
+            Tag::PUBK => b"PUBK",
+            Tag::RADI => b"RADI",
+            Tag::ROOT => b"ROOT",
+            Tag::SIG => b"SIG\x00",
+            Tag::SREP => b"SREP",
         }
     }
 }
