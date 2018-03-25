int08h/roughenough

commit 9656fdab0f702ccd784a2e50eabcf94809bc31b5

Check that value offset does not extend pass end of message

src/message.rs
@@ -112,14 +112,19 @@ impl RtMessage {
         // as an offset from the end of the header
         let msg_end = bytes.len() - header_end;
 
-        assert_eq!(offsets.len(), tags.len() - 1);
-
         for (tag, (value_start, value_end)) in tags.into_iter().zip(
             once(&0)
                 .chain(offsets.iter())
-                .zip(offsets.iter().chain(once(&msg_end))),
+                .zip(offsets.iter().chain(once(&msg_end)))
         ) {
-            let value = bytes[(header_end + value_start)..(header_end + value_end)].to_vec();
+            let start_idx = header_end + value_start;
+            let end_idx = header_end + value_end;
+
+            if end_idx > msg_end || start_idx > end_idx {
+                return Err(Error::InvalidValueLength(tag, end_idx as u32));
+            }
+
+            let value = bytes[start_idx..end_idx].to_vec();
             rt_msg.add_field(tag, &value)?;
         }
 

src/error.rs
@@ -25,6 +25,12 @@ pub enum Error {
     /// The associated byte sequence does not correspond to a valid Roughtime tag.
     InvalidTag(Box<[u8]>),
 
+    /// Invalid number of tags specified
+    InvalidNumTags(u32),
+
+    /// Tag value length exceeds length of source bytes
+    InvalidValueLength(Tag, u32),
+
     /// Encoding failed. The associated `std::io::Error` should provide more information.
     EncodingFailure(std::io::Error),
 
@@ -36,9 +42,6 @@ pub enum Error {
 
     /// Offset is outside of valid message range
     InvalidOffsetValue(u32),
-
-    /// Invalid number of tags specified 
-    InvalidNumTags(u32),
 
     /// Could not convert bytes to message because bytes were too short
     MessageTooShort,

commit ed267f79b0bc070c5c63e5936db79e9d5aced30c

Fix incorrect range check in multi_tag_message()
9656fda introduced a buggy range check when ensuring that the
value's end offset does not extend past the end of the payload.

src/message.rs
@@ -145,7 +145,7 @@ impl RtMessage {
             let start_idx = header_end + value_start;
             let end_idx = header_end + value_end;
 
-            if end_idx > msg_end || start_idx > end_idx {
+            if end_idx > bytes_len || start_idx > end_idx {
                 return Err(Error::InvalidValueLength(tag, end_idx as u32));
             }
 
@@ -342,6 +342,9 @@ mod test {
 
         // Entire message was read
         assert_eq!(encoded.position(), 72);
+
+        // Round-trip single-tag message
+        RtMessage::from_bytes(&msg.encode().unwrap()).unwrap();
     }
 
     #[test]
@@ -392,6 +395,9 @@ mod test {
 
         // Everything was read
         assert_eq!(encoded.position() as usize, msg.encoded_size());
+
+        // Round-trip multi-tag message
+        RtMessage::from_bytes(&msg.encode().unwrap()).unwrap();
     }
 
     #[test]