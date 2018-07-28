aa12046d784859505d7a5f4c182414fc4ca5f32a
Stuart Stock <stuart@int08h.com>
2018-07-28 17:53:27 -0500

    bump version constant to 1.0.4, oops

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@ pub use tag::Tag;
 pub use message::RtMessage;
 
 /// Version of Roughenough
-pub const VERSION: &str = "1.0.3";
+pub const VERSION: &str = "1.0.4";
 
 //  Constants and magic numbers of the Roughtime protocol
 
