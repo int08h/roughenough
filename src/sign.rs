5d48837810a635013937455529eff30d10822cc3
Stuart Stock <stuart@int08h.com>
2022-03-27 16:02:37 -0500

    Make tests pass by updating signing test to use correct base 64 decoder.

--- src/sign.rs
+++ src/sign.rs
@@ -127,12 +127,12 @@ mod test {
 
     #[test]
     fn verify_ed25519_sig_on_empty_message() {
-        let pubkey = hex::decode(
-            "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a",
+        let pubkey = HEX.decode(
+            "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a".as_ref(),
         ).unwrap();
 
-        let signature = hex::decode(
-            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b"
+        let signature = HEX.decode(
+            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b".as_ref()
         ).unwrap();
 
         let v = Verifier::new(&pubkey);
@@ -142,14 +142,14 @@ mod test {
 
     #[test]
     fn verify_ed25519_sig() {
-        let pubkey = hex::decode(
-            "c0dac102c4533186e25dc43128472353eaabdb878b152aeb8e001f92d90233a7",
+        let pubkey = HEX.decode(
+            "c0dac102c4533186e25dc43128472353eaabdb878b152aeb8e001f92d90233a7".as_ref(),
         ).unwrap();
 
-        let message = hex::decode("5f4c8989").unwrap();
+        let message = HEX.decode("5f4c8989".as_ref()).unwrap();
 
-        let signature = hex::decode(
-            "124f6fc6b0d100842769e71bd530664d888df8507df6c56dedfdb509aeb93416e26b918d38aa06305df3095697c18b2aa832eaa52edc0ae49fbae5a85e150c07"
+        let signature = HEX.decode(
+            "124f6fc6b0d100842769e71bd530664d888df8507df6c56dedfdb509aeb93416e26b918d38aa06305df3095697c18b2aa832eaa52edc0ae49fbae5a85e150c07".as_ref()
         ).unwrap();
 
         let mut v = Verifier::new(&pubkey);
@@ -160,11 +160,11 @@ mod test {
 
     #[test]
     fn sign_ed25519_empty_message() {
-        let seed = hex::decode("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60")
+        let seed = HEX.decode("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60".as_ref())
             .unwrap();
 
-        let expected_sig = hex::decode(
-            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b"
+        let expected_sig = HEX.decode(
+            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b".as_ref()
         ).unwrap();
 
         let mut s = Signer::from_seed(&seed);
@@ -174,13 +174,13 @@ mod test {
 
     #[test]
     fn sign_ed25519_message() {
-        let seed = hex::decode("0d4a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9")
+        let seed = HEX.decode("0d4a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9".as_ref())
             .unwrap();
 
-        let message = hex::decode("cbc77b").unwrap();
+        let message = HEX.decode("cbc77b".as_ref()).unwrap();
 
-        let expected_sig = hex::decode(
-            "d9868d52c2bebce5f3fa5a79891970f309cb6591e3e1702a70276fa97c24b3a8e58606c38c9758529da50ee31b8219cba45271c689afa60b0ea26c99db19b00c"
+        let expected_sig = HEX.decode(
+            "d9868d52c2bebce5f3fa5a79891970f309cb6591e3e1702a70276fa97c24b3a8e58606c38c9758529da50ee31b8219cba45271c689afa60b0ea26c99db19b00c".as_ref()
         ).unwrap();
 
         let mut s = Signer::from_seed(&seed);
@@ -191,7 +191,7 @@ mod test {
 
     #[test]
     fn sign_verify_round_trip() {
-        let seed = hex::decode("334a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9")
+        let seed = HEX.decode("334a05b07352a5436e180356da0ae6efa0345ff7fb1572575772e8005ed978e9".as_ref())
             .unwrap();
 
         let message = "Hello world".as_bytes();
